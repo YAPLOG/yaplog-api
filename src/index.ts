51beb17174cc09a0c33009dc99cd0c32ff281cdf
Timberzzman <leloupandco@gmail.com>
2022-11-17 17:53:59 +0100

    feat: update to implement deployment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify';
 
-const server = fastify();
+const server = fastify({
+  logger: true,
+});
 
 server.get('/health', async (_request, reply) => reply.send(200));
 
@@ -8,10 +10,24 @@ const serverConfig = {
   port: parseInt(process.env['PORT'] || '8080', 10),
 };
 
+if (process.env['NODE_ENV'] === 'production') {
+  ['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, () => server.close().then((err) => {
+      console.log(`close application on ${signal}`);
+      process.exit(err ? 1 : 0);
+    }));
+  });
+}
+
 server.listen(serverConfig, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
+});
+
+process.on('unhandledRejection', (e) => {
+  console.error(e);
+  process.exit(1);
 });
